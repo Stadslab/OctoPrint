4ae787b69ad2163391a5fcfac0bbea081ec725f8
Marco <marcovannoord+github@gmail.com>
2015-05-07 20:52:03 +0200

    removed bug

--- StadslabInstall.sh
+++ StadslabInstall.sh
@@ -1,5 +1,6 @@
 #!/bin/bash          
 echo Starting install of Stadslab Printcloud!
+#need to add switching to devel branch
 sudo usermod -a -G tty pi
 sudo usermod -a -G dialout pi
 sudo rm -r ~/.octoprint1
@@ -10,9 +11,9 @@ sudo rm -r ~/.octoprint3
 sudo mkdir ~/.octoprint3
 sudo rm -r ~/.octoprint4
 sudo mkdir ~/.octoprint4
-sudo cp config/.octoprint ./octoprint1
-sudo cp config/.octoprint ./octoprint2
-sudo cp config/.octoprint ./octoprint3
-sudo cp config/.octoprint ./octoprint4
+sudo cp .octoprint ./octoprint1
+sudo cp .octoprint ./octoprint2
+sudo cp .octoprint ./octoprint3
+sudo cp .octoprint ./octoprint4
 
 
