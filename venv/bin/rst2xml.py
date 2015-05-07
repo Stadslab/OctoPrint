a7afe45ec8b69281c5cfde77d8881aee57ba8266
Marco <marcovannoord@gmail.com>
2015-05-07 18:07:12 +0000

    local config added

---
+++ venv/bin/rst2xml.py
@@ -0,0 +1,5 @@
+#!/home/pi/OctoPrint/venv/bin/python
+# EASY-INSTALL-SCRIPT: 'docutils==0.12','rst2xml.py'
+__requires__ = 'docutils==0.12'
+import pkg_resources
+pkg_resources.run_script('docutils==0.12', 'rst2xml.py')
